4353f6705721991cd602df1490a397b369787d48
Julien <julien.linder@gmail.com>
2016-12-30 16:51:24 +0100

    initial commit

--- js/app.js
+++ js/app.js
@@ -32,3 +32,15 @@ var customers = [
 ]
 
 // Write you're code here. Enjoy !
+    var customers1 = customers[0]
+    console.log(customers1.id, customers1.first_name, customers1.last_name, customers1.company, customers1.role, customers1.phone, customers1.email, customers1.description);
+
+    var customers2 = customers[1]
+    console.log(customers1.id, customers1.first_name, customers1.last_name, customers1.company, customers1.role, customers1.phone, customers1.email, customers1.description);
+
+    var customers3 = customers[2]
+    console.log(customers1.id, customers1.first_name, customers1.last_name, customers1.company, customers1.role, customers1.phone, customers1.email, customers1.description);
+
+    document.getElementById("customers1").innerHTML = customers1.id +"<br>" +customers1.first_name +"<br>" +customers1.last_name +"<br>" +customers1.company +"<br>" +customers1.role +"<br>" +customers1.phone +"<br>" +customers1.email +"<br>"+customers1.description; 
+    document.getElementById("customers2").innerHTML = customers2.id +"<br>" +customers2.first_name +"<br>" +customers2.last_name +"<br>" +customers2.company +"<br>" +customers2.role +"<br>" +customers2.phone +"<br>" +customers2.email +"<br>"+customers2.description; 
+    document.getElementById("customers3").innerHTML = customers3.id +"<br>" +customers3.first_name +"<br>" +customers3.last_name +"<br>" +customers3.company +"<br>" +customers3.role +"<br>" +customers3.phone +"<br>" +customers3.email +"<br>"+ customers3.description; 
